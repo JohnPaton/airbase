60dcdf707e8bec08f1a44bc07ac5be41d73a731d
Alvaro Valdebenito <alvarov@met.no>
2022-01-17 14:26:18 +0100

    specialiced fetch helper functions

--- airbase/_fetch.py
+++ airbase/_fetch.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import sys
 from typing import Iterator
 
 from requests import Response, Session
@@ -13,3 +14,33 @@ def fetch_urls(
     with Session() as session:
         for url in tqdm(urls, leave=True, disable=not progress):
             yield session.get(url, timeout=timeout)
+
+
+def fetch_json(url: str, *, timeout: float | None = None) -> dict:
+    r, *_ = fetch_urls(url, timeout=timeout)
+    r.raise_for_status()
+    return r.json()
+
+
+def fetch_text(url: str, *, encoding: str | None = None) -> str:
+    (text, url), *_ = fetch_all_text(url, encoding=encoding)
+    return text
+
+
+def fetch_all_text(
+    *url: str,
+    progress: bool = False,
+    encoding: str | None = None,
+    raise_for_status: bool = True,
+) -> Iterator[tuple[str, str]]:
+    for r in fetch_urls(*url, progress=progress):
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            if not raise_for_status:
+                print(f"Warning: {e}", file=sys.stderr)
+                continue
+            raise
+        if encoding is not None:
+            r.encoding = encoding
+        yield r.text, r.url
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,15 +1,22 @@
+import json
 import re
 from typing import Iterator
 
 import pytest
 from responses import Response
 
-from airbase._fetch import fetch_urls
+from airbase._fetch import fetch_json, fetch_text, fetch_urls
+
+JSON_PAYLOAD = {"payload": "test"}
 
 
 @pytest.fixture
 def echo_url(responses) -> Iterator[str]:
-    r = Response(method="GET", url=re.compile(r"https://echo\.test/.*"))
+    r = Response(
+        method="GET",
+        url=re.compile(r"https://echo\.test/.*"),
+        json=JSON_PAYLOAD,
+    )
     responses.add(r)
     yield "https://echo.test"
     responses.remove(r)
@@ -23,3 +30,15 @@ def test_fetch_urls(echo_url: str):
     responses = list(fetch_urls(*urls))
     assert len(responses) == len(urls)
     assert [r.url for r in responses] == urls
+
+
+def test_fetch_json(echo_url: str):
+    url = f"{echo_url}/this/is/test.json"
+    json = fetch_json(url)
+    assert json == JSON_PAYLOAD
+
+
+def test_fetch_text(echo_url: str):
+    url = f"{echo_url}/this/is/test.txt"
+    text = fetch_text(url)
+    assert json.loads(text) == JSON_PAYLOAD
