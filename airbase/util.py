dc6d6c87cd2b35f8dcee6487cf9fc91dddc61f04
Alvaro Valdebenito <alvarov@met.no>
2022-01-24 08:49:03 +0100

    unused type:ignore

--- airbase/util.py
+++ airbase/util.py
@@ -23,7 +23,7 @@ def string_safe_list(obj: str | Iterable[str]) -> list[str]:
     ...
 
 
-def string_safe_list(obj):  # type:ignore[no-untyped-def]
+def string_safe_list(obj):
     """
     Turn an (iterable) object into a list. If it is a string or not
     iterable, put the whole object into a list of length 1.
