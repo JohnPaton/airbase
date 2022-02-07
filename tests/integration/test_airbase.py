4afcc98edbe0607284df393b4b39733d1a48e97d
Alvaro Valdebenito <alvarov@met.no>
2022-02-07 09:26:08 +0100

    clean up integrations tests

--- tests/integration/test_airbase.py
+++ tests/integration/test_airbase.py
@@ -1,28 +1,17 @@
 import os
 
 import pytest
-from aioresponses import aioresponses
 
 import airbase
-
-
-from ..resources import CSV_RESPONSE, METADATA_RESPONSE
+from tests.resources import CSV_RESPONSE, METADATA_RESPONSE
 
 
 @pytest.fixture(scope="module")
 def client():
-    """Return an initialized AirbaseClient"""
-    prefixes = [
-        airbase.resources.E1A_SUMMARY_URL,
-        "http://fme.discomap.eea.europa.eu/",
-        "https://ereporting.blob.core.windows.net/",
-        airbase.resources.METADATA_URL,
-    ]
-    with aioresponses(passthrough=prefixes):
-        yield airbase.AirbaseClient(connect=True)
+    """Return initialized AirbaseClient instance"""
+    return airbase.AirbaseClient(connect=True)
 
 
-@pytest.mark.withoutresponses
 def test_client_connects(client: airbase.AirbaseClient):
     assert client.all_countries is not None
     assert client.all_pollutants is not None
@@ -30,7 +19,6 @@ def test_client_connects(client: airbase.AirbaseClient):
     assert client.search_pollutant("O3") is not None
 
 
-@pytest.mark.withoutresponses
 def test_download_to_directory(client: airbase.AirbaseClient, tmpdir):
     r = client.request(
         country=["AD", "BE"], pl="CO", year_from="2017", year_to="2017"
@@ -39,7 +27,6 @@ def test_download_to_directory(client: airbase.AirbaseClient, tmpdir):
     assert os.listdir(str(tmpdir)) != []
 
 
-@pytest.mark.withoutresponses
 def test_download_to_file(client: airbase.AirbaseClient, tmpdir):
     r = client.request(
         country="CY", pl=["As", "NO2"], year_from="2014", year_to="2014"
@@ -55,7 +42,6 @@ def test_download_to_file(client: airbase.AirbaseClient, tmpdir):
     assert headers_downloaded == headers_expected
 
 
-@pytest.mark.withoutresponses
 def test_download_metadata(client: airbase.AirbaseClient, tmpdir):
     client.download_metadata(str(tmpdir / "metadata.tsv"))
     assert os.path.exists(str(tmpdir / "metadata.tsv"))
