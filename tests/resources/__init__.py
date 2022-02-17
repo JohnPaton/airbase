e088c2a63249f41612cd8fd56902b14d173d89c5
Alvaro Valdebenito <alvarov@met.no>
2022-02-17 12:24:47 +0100

    use resources.read_text

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -1,14 +1,12 @@
 import json
 from importlib import resources
 
-with resources.path(__package__, "summary.json") as path:
-    SUMMARY = json.loads(path.read_text())
+SUMMARY = json.loads(resources.read_text(__package__, "summary.json"))
 
-with resources.path(__package__, "csv_links_response.txt") as path:
-    CSV_LINKS_RESPONSE_TEXT = path.read_text()
+CSV_LINKS_RESPONSE_TEXT = resources.read_text(
+    __package__, "csv_links_response.txt"
+)
 
-with resources.path(__package__, "csv_response.csv") as path:
-    CSV_RESPONSE = path.read_text()
+CSV_RESPONSE = resources.read_text(__package__, "csv_response.csv")
 
-with resources.path(__package__, "metadata.tsv") as path:
-    METADATA_RESPONSE = path.read_text()
+METADATA_RESPONSE = resources.read_text(__package__, "metadata.tsv")
