c4f9f331c8a56685aab93d4829558d82911d12c3
Alvaro Valdebenito <alvarov@met.no>
2022-02-17 14:03:41 +0100

    add missing types

--- airbase/util.py
+++ airbase/util.py
@@ -1,6 +1,9 @@
 """Utility functions for processing the raw Portal responses, url templating, etc."""
 
-import datetime
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Iterable, overload
 
 from .resources import (
     ALL_SOURCES,
@@ -10,7 +13,17 @@ from .resources import (
 )
 
 
-def string_safe_list(obj):
+@overload
+def string_safe_list(obj: None) -> list[None]:  # pragma: no cover
+    ...
+
+
+@overload
+def string_safe_list(obj: str | Iterable[str]) -> list[str]:  # pragma: no cover
+    ...
+
+
+def string_safe_list(obj: str | Iterable[str] | None) -> list[str] | list[None]:
     """
     Turn an (iterable) object into a list. If it is a string or not
     iterable, put the whole object into a list of length 1.
@@ -18,44 +31,47 @@ def string_safe_list(obj):
     :param obj:
     :return list:
     """
-    if isinstance(obj, str) or not hasattr(obj, "__iter__"):
+    if isinstance(obj, str):
         return [obj]
-    else:
-        return list(obj)
+    if obj is None:
+        return [obj]
+    return list(obj)
 
 
-def countries_from_summary(summary):
+def countries_from_summary(summary: list[dict[str, str]]) -> list[str]:
     """
     Get the list of unique countries from the summary.
 
-    :param list[dict] summary: The E1a summary.
+    :param summary: The E1a summary.
 
-    :return list[str]: The available countries.
+    :return: The available countries.
     """
     return list({d["ct"] for d in summary})
 
 
-def pollutants_from_summary(summary):
+def pollutants_from_summary(summary: list[dict[str, str]]) -> dict[str, str]:
     """
     Get the list of unique pollutants from the summary.
 
-    :param list[dict] summary: The E1a summary.
+    :param summary: The E1a summary.
 
-    :return dict: The available pollutants, with name ("pl") as key
+    :return: The available pollutants, with name ("pl") as key
         and pollutant number ("shortpl") as value.
     """
     return {d["pl"]: d["shortpl"] for d in summary}
 
 
-def pollutants_per_country(summary):
+def pollutants_per_country(
+    summary: list[dict[str, str]]
+) -> dict[str, list[dict[str, str]]]:
     """
     Get the available pollutants per country from the summary.
 
-    :param list[dict] summary: The E1a summary.
+    :param summary: The E1a summary.
 
-    :return dict[list[dict]]: All available pollutants per country.
+    :return: All available pollutants per country.
     """
-    output = dict()
+    output: dict[str, list[dict[str, str]]] = dict()
 
     for d in summary.copy():
         country = d.pop("ct")
@@ -69,38 +85,36 @@ def pollutants_per_country(summary):
 
 
 def link_list_url(
-    country,
-    shortpl=None,
-    year_from="2013",
-    year_to=CURRENT_YEAR,
-    source="All",
-    update_date=None,
-):
+    country: str | None,
+    shortpl: str | None = None,
+    year_from: str = "2013",
+    year_to: str = CURRENT_YEAR,
+    source: str = "All",
+    update_date: str | datetime | None = None,
+) -> str:
     """
     Generate the URL where the download links for a query can be found.
 
-    :param str country: The 2-letter country code. See
+    :param country: The 2-letter country code. See
         AirbaseClient.countries for options.
-    :param str shortpl: (optional) The pollutant number. Leave blank to
+    :param shortpl: (optional) The pollutant number. Leave blank to
         get all pollutants. See AirbaseClient.pollutants_per_country for
         options.
-    :param str year_from: (optional) The first year of data. Can not be
+    :param year_from: (optional) The first year of data. Can not be
         earlier than 2013. Default 2013.
-    :param str year_to: (optional) The last year of data. Can not be
+    :param year_to: (optional) The last year of data. Can not be
         later than the current year. Default <current year>.
-    :param str source: (optional) One of "E1a", "E2a" or "All". E2a
+    :param source: (optional) One of "E1a", "E2a" or "All". E2a
         (UTD) data are only available for years where E1a data have not
         yet been delivered (this will normally be the most recent year).
         Default "All".
-    :param str|datetime update_date: (optional). Format
+    :param update_date: (optional). Format
         "yyyy-mm-dd hh:mm:ss". To be used when only files created or
         updated after a certain date is of interest.
 
-    :return str: The URL which will yield the list of relevant CSV
+    :return: The URL which will yield the list of relevant CSV
         download links.
     """
-    shortpl = shortpl or ""
-
     if int(year_from) < 2013:
         raise ValueError("'year_from' must be at least 2013")
     year_from = str(int(year_from))
@@ -109,7 +123,7 @@ def link_list_url(
         raise ValueError("'year_to' must be at most " + str(CURRENT_YEAR))
     year_to = str(int(year_to))
 
-    if isinstance(update_date, datetime.datetime):
+    if isinstance(update_date, datetime):
         update_date = update_date.strftime(DATE_FMT)
     update_date = update_date or ""
 
@@ -118,8 +132,8 @@ def link_list_url(
     source = source or ""
 
     return LINK_LIST_URL_TEMPLATE.format(
-        country=country,
-        shortpl=shortpl,
+        country=country or "",
+        shortpl=shortpl or "",
         year_from=year_from,
         year_to=year_to,
         source=source,
