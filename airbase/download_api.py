06bea3db0eb9ed24e9390c648d1a6c1f6b2fd4f1
Alvaro Valdebenito <alvarov@met.no>
2024-08-23 14:15:19 +0200

    city names from Download API

--- airbase/download_api.py
+++ airbase/download_api.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 from collections import defaultdict
 from typing import Literal
+from warnings import warn
 
 import aiohttp
 
@@ -16,7 +17,7 @@ COUNTRY_CODES = set(
 )
 
 
-async def get_json_from_api(
+async def json_from_get_api(
     entry_point: Literal["Country", "Property"],
     *,
     timeout: float | None = None,
@@ -42,9 +43,37 @@ async def get_json_from_api(
             return payload
 
 
+async def json_from_post_api(
+    entry_point: Literal["City"],
+    data: tuple[str, ...] | list[str],
+    *,
+    timeout: float | None = None,
+    encoding: str | None = None,
+) -> list[dict[str, str]]:
+    """
+    post request to an specific Download API entry point and return decoded JSON
+
+    :param entry_point: Download API entry point
+    :param data:
+    :param timeout: maximum time to complete request (seconds)
+    :param encoding: text encoding used for decoding the response's body
+
+    :return: decoded JSON from response's body
+    """
+
+    timeout_ = aiohttp.ClientTimeout(total=timeout)
+    async with aiohttp.ClientSession(timeout=timeout_) as session:
+        async with session.post(
+            f"{API_SERVICE_ROOT}/{entry_point}", json=data, ssl=False
+        ) as r:
+            r.raise_for_status()
+            payload: list[dict[str, str]] = await r.json(encoding=encoding)
+            return payload
+
+
 def countries() -> list[str]:
     """request country codes from API"""
-    payload = asyncio.run(get_json_from_api("Country"))
+    payload = asyncio.run(json_from_get_api("Country"))
     return [country["countryCode"] for country in payload]
 
 
@@ -61,9 +90,27 @@ def pollutant_id_from_url(url: str) -> int:
 
 def pollutants() -> defaultdict[str, set[int]]:
     """requests pollutants id and notation from API"""
-    payload = asyncio.run(get_json_from_api("Property"))
+    payload = asyncio.run(json_from_get_api("Property"))
     ids: defaultdict[str, set[int]] = defaultdict(set)
     for poll in payload:
         key, val = poll["notation"], pollutant_id_from_url(poll["id"])
         ids[key].add(val)
     return ids
+
+
+def cities(*countries: str) -> defaultdict[str, set[str]]:
+    """city names id and notation from API"""
+    if not COUNTRY_CODES.issuperset(countries):
+        unknown = sorted(set(countries) - COUNTRY_CODES)
+        warn(
+            f"Unknown country code(s) {', '.join(unknown)}",
+            UserWarning,
+            stacklevel=2,
+        )
+
+    payload = asyncio.run(json_from_post_api("City", countries))
+    cities: defaultdict[str, set[str]] = defaultdict(set)
+    for city in payload:
+        key, val = city["countryCode"], city["cityName"]
+        cities[key].add(val)
+    return cities
--- tests/test_download_api.py
+++ tests/test_download_api.py
@@ -1,6 +1,8 @@
 from itertools import chain
 
-from airbase.download_api import COUNTRY_CODES, countries, pollutants
+import pytest
+
+from airbase.download_api import COUNTRY_CODES, cities, countries, pollutants
 
 
 def test_countries():
@@ -18,3 +20,50 @@ def test_pollutants():
 
     for poll, id in {"PM10": 5, "O3": 7, "NO2": 8, "SO2": 1}.items():
         assert pollutants_.get(poll) == {id}, f"unknown {poll} {id=}"
+
+
+def test_cities():
+    known_cities = dict(
+        IS={"Reykjavik"},
+        NO={
+            "Bergen",
+            "Kristiansand",
+            "Oslo",
+            "Stavanger",
+            "Tromsø",
+            "Trondheim",
+        },
+        SE={
+            "Borås",
+            "Göteborg",
+            "Helsingborg",
+            "Jönköping",
+            "Linköping",
+            "Lund",
+            "Malmö",
+            "Norrköping",
+            "Örebro",
+            "Sodertalje",
+            "Stockholm (greater city)",
+            "Umeå",
+            "Uppsala",
+            "Västerås",
+        },
+        FI={
+            "Helsinki / Helsingfors (greater city)",
+            "Jyväskylä",
+            "Kuopio",
+            "Lahti / Lahtis",
+            "Oulu",
+            "Tampere / Tammerfors",
+            "Turku / Åbo",
+        },
+    )
+    for country, cities_ in cities(*known_cities).items():
+        assert cities_ <= known_cities[country], f"missing cities on {country}"
+
+
+def test_cities_invalid_country():
+    countries = ("Norway", "Finland", "USA")
+    with pytest.warns(UserWarning, match="Unknown country"):
+        assert not cities(*countries), "dict is not empty"
