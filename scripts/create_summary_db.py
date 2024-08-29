e8666301cb8e72009daade94955c84ccb63e9213
Alvaro Valdebenito <alvarov@met.no>
2024-08-29 06:51:44 +0200

    clean up DB script

--- scripts/create_summary_db.py
+++ scripts/create_summary_db.py
@@ -3,10 +3,15 @@ from __future__ import annotations
 
 import json
 import sqlite3
+import subprocess
 from contextlib import closing
 from pathlib import Path
 
+from airbase.download_api.api_client import CityDict, CountryDict, PropertyDict
 from airbase.download_api.download_session import pollutant_id_from_url
+
+BASE_URL = "https://eeadmz1-downloads-api-appservice.azurewebsites.net"
+
 
 CREATE_DB = """
 DROP TABLE IF EXISTS city;
@@ -57,29 +62,49 @@ VALUES (:notation, :id, :url);
 """
 
 
-def main(
-    db_path: Path = Path("airbase/summary/summary2.sqlite"),
-    data_path: Path = Path("tests/resources"),
-):
-    city: list[dict] = json.loads(data_path.joinpath("city.json").read_text())
-    country: list[dict] = json.loads(
-        data_path.joinpath("country.json").read_text()
-    )
-    missing_counties = set(country["countryCode"] for country in country)
-    missing_counties -= set(country["countryCode"] for country in city)
-    for country_code in sorted(missing_counties):
-        city.append(dict(countryCode=country_code, cityName=None))
+def main(db_path: Path = Path("airbase/summary/summary.sqlite")):
+    country = country_json()
+    country_codes = set(country["countryCode"] for country in country)
 
-    property: list[dict] = json.loads(
-        data_path.joinpath("property.json").read_text()
-    )
+    city = city_json(*country_codes)
+    country_codes -= set(country["countryCode"] for country in city)
+    for country_code in sorted(country_codes):
+        city.append(dict(countryCode=country_code, cityName=None))  # type:ignore[typeddict-item]
+
+    property = property_json()
     for poll in property:
-        poll.update(url=poll["id"], id=pollutant_id_from_url(poll["id"]))
+        poll.update(url=poll["id"], id=pollutant_id_from_url(poll["id"]))  # type:ignore[call-arg]
 
     with sqlite3.connect(db_path) as db, closing(db.cursor()) as cur:
         cur.executescript(CREATE_DB)
         cur.executemany(INSERT_CITY_JSON, city)
         cur.executemany(INSERT_PROPERTY_JSON, property)
+
+
+def country_json() -> list[CountryDict]:
+    cmd = f"curl -s -X 'GET' '{BASE_URL}/Country' -H 'accept: text/plain'"
+    payload = subprocess.check_output(cmd, shell=True, encoding="UTF-8")
+    assert payload, "no data"
+    return json.loads(payload)  # type:ignore[no-any-return]
+
+
+def city_json(*country_codes: str) -> list[CityDict]:
+    cmd = (
+        f"curl -s -X 'POST' '{BASE_URL}/City' "
+        " -H 'accept: text/plain'"
+        " -H 'Content-Type: application/json'"
+        f" -d '{json.dumps(sorted(country_codes))}'"
+    )
+    payload = subprocess.check_output(cmd, shell=True, encoding="UTF-8")
+    assert payload, "no data"
+    return json.loads(payload)  # type:ignore[no-any-return]
+
+
+def property_json() -> list[PropertyDict]:
+    cmd = f"curl -s -X 'GET' '{BASE_URL}/Property'  -H 'accept: text/plain'"
+    payload = subprocess.check_output(cmd, shell=True)
+    assert payload, "no data"
+    return json.loads(payload)  # type:ignore[no-any-return]
 
 
 if __name__ == "__main__":
