e1482852a675d66b6ffd074f71e8812dd5674545
Alvaro Valdebenito <alvarov@met.no>
2024-08-29 15:27:45 +0200

    cheaper CLI integration tests

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 
 import pytest
@@ -9,20 +11,37 @@ runner = CliRunner()
 
 
 @pytest.mark.parametrize(
-    "cmd,country,city,num",
+    "cmd,country,city,pollutant,expected",
     (
-        pytest.param("historical", "MT", "Valletta", 22, id="historical"),
-        pytest.param("verified", "MT", "Valletta", 48, id="verified"),
-        pytest.param("unverified", "MT", "Valletta", 48, id="unverified"),
+        pytest.param(
+            "historical", "MT", "Valletta", "PM2.5",
+            {"SPO-MT00005_06001_100.parquet"},
+            id="historical",
+        ),
+        pytest.param(
+            "verified", "MT", "Valletta", "O3",
+            {"SPO-MT00003_00007_100.parquet", "SPO-MT00005_00007_100.parquet"},
+            id="verified"),
+        pytest.param(
+            "unverified", "MT", "Valletta", "PM10",
+            {"SPO-MT00003_00005_100.parquet", "SPO-MT00003_00005_101.parquet",
+             "SPO-MT00005_00005_100.parquet", "SPO-MT00005_00005_101.parquet"},
+            id="unverified"
+        ),
     ),
-)
-def test_download(cmd: str, country: str, city: str, num: int, tmp_path: Path):
-    options = (
-        f"{cmd} --quiet --country {country} --city {city} --path {tmp_path}"
-    )
+)  # fmt:skip
+def test_download(
+    cmd: str,
+    country: str,
+    city: str,
+    pollutant: str,
+    expected: set[str],
+    tmp_path: Path,
+):
+    options = f"{cmd} --quiet --country {country} --city {city} --pollutant {pollutant} --path {tmp_path}"
     with runner.isolated_filesystem(temp_dir=tmp_path):
         result = runner.invoke(main, options.split())
         assert result.exit_code == 0
 
-    files = sorted(tmp_path.glob(f"{country}/*.parquet"))
-    assert len(files) >= num
+    files = set(path.name for path in tmp_path.glob(f"{country}/*.parquet"))
+    assert files >= expected > set()
