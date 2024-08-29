5dafcffe816a3ceeb0f09f7547e11151bec4b23b
Alvaro Valdebenito <alvarov@met.no>
2024-08-29 21:27:30 +0200

    unused fetcher

--- airbase/fetch.py
+++ airbase/fetch.py
@@ -3,20 +3,8 @@
 from __future__ import annotations
 
 import asyncio
-import warnings
-from pathlib import Path
-from types import SimpleNamespace
-from typing import AsyncIterator, Awaitable, overload
 
-import aiofiles
 import aiohttp
-from tqdm import tqdm
-
-DEFAULT = SimpleNamespace(
-    progress=False,
-    raise_for_status=True,
-    max_concurrent=10,
-)
 
 
 def fetch_text(
@@ -44,89 +32,3 @@ def fetch_text(
 
     text = asyncio.run(fetch())
     return text
-
-
-@overload
-def fetcher(
-    urls: list[str],
-    *,
-    encoding: str | None = None,
-    progress: bool = DEFAULT.progress,
-    raise_for_status: bool = DEFAULT.raise_for_status,
-    max_concurrent: int = DEFAULT.max_concurrent,
-) -> AsyncIterator[str]:  # pragma: no cover
-    ...
-
-
-@overload
-def fetcher(
-    urls: dict[str, Path],
-    *,
-    encoding: str | None = None,
-    progress: bool = DEFAULT.progress,
-    raise_for_status: bool = DEFAULT.raise_for_status,
-    max_concurrent: int = DEFAULT.max_concurrent,
-) -> AsyncIterator[Path]:  # pragma: no cover
-    ...
-
-
-async def fetcher(
-    urls: list[str] | dict[str, Path],
-    *,
-    encoding: str | None = None,
-    progress: bool = DEFAULT.progress,
-    raise_for_status: bool = DEFAULT.raise_for_status,
-    max_concurrent: int = DEFAULT.max_concurrent,
-) -> AsyncIterator[str | Path]:
-    """Request multiple urls and write request text into individual paths
-    it a `dict[url, path]` is provided, or return the decoded text from each request
-    if only a `list[url]` is provided.
-
-    :param urls: requested urls
-    :param encoding: text encoding used for decoding each response's body
-    :param progress: show progress bar
-    :param raise_for_status: Raise exceptions if download links
-        return "bad" HTTP status codes. If False,
-        a :py:func:`warnings.warn` will be issued instead.
-    :param max_concurrent: maximum concurrent requests
-
-    :return: url text or path to downloaded text, one by one as the requests are completed
-    """
-
-    async with aiohttp.ClientSession() as session:
-        semaphore = asyncio.Semaphore(max_concurrent)
-
-        async def fetch(url: str) -> str:
-            """retrieve text, nothing more"""
-            async with semaphore:
-                async with session.get(url, ssl=False) as r:
-                    r.raise_for_status()
-                    text: str = await r.text(encoding=encoding)
-                    return text
-
-        async def download(url: str, path: Path) -> Path:
-            """retrieve text and write into path"""
-            async with semaphore:
-                async with session.get(url, ssl=False) as r:
-                    r.raise_for_status()
-                    text: str = await r.text(encoding=encoding)
-                async with aiofiles.open(str(path), mode="w") as f:
-                    await f.write(text)
-                return path
-
-        jobs: list[Awaitable[str | Path]]
-        if isinstance(urls, dict):
-            jobs = [download(url, path) for url, path in urls.items()]
-        else:
-            jobs = [fetch(url) for url in urls]
-        with tqdm(total=len(jobs), leave=True, disable=not progress) as p_bar:
-            for result in asyncio.as_completed(jobs):
-                p_bar.update(1)
-                try:
-                    yield await result
-                except asyncio.CancelledError:
-                    continue
-                except aiohttp.ClientResponseError as e:
-                    if raise_for_status:
-                        raise
-                    warnings.warn(str(e), category=RuntimeWarning)
