8cf7b5e0cd6491f04a296a6bb79e0080d9d0cbcb
Alvaro Valdebenito <alvarov@met.no>
2024-09-01 21:41:35 +0200

    private session and semaphore

--- airbase/download_api/concrete_api_client.py
+++ airbase/download_api/concrete_api_client.py
@@ -42,17 +42,17 @@ class Client(AbstractAPIClient):
     ) -> None:
         self.timeout = timeout
         self.max_concurrent = max_concurrent
-        self.session: aiohttp.ClientSession | None = None
-        self.semaphore: asyncio.Semaphore | None = None
+        self.__session: aiohttp.ClientSession | None = None
+        self.__semaphore: asyncio.Semaphore | None = None
 
     async def __aenter__(self) -> Self:
-        self.session = aiohttp.ClientSession(
+        self.__session = aiohttp.ClientSession(
             connector=aiohttp.TCPConnector(
                 ssl=False, limit=self.max_concurrent
             ),
             timeout=aiohttp.ClientTimeout(self.timeout),
         )
-        self.semaphore = asyncio.Semaphore(self.max_concurrent)
+        self.__semaphore = asyncio.Semaphore(self.max_concurrent)
         return self
 
     async def __aexit__(
@@ -61,59 +61,75 @@ class Client(AbstractAPIClient):
         exc_val: BaseException | None,
         exc_tb: TracebackType | None,
     ) -> None:
-        assert self.session is not None, "outside context manager"
-        await self.session.close()
-        self.session = self.semaphore = None
+        await self.__session.close()  # type:ignore[union-attr]
+        self.__session = self.__semaphore = None
+
+    @property
+    def _session(self) -> aiohttp.ClientSession:
+        """private `aiohttp.ClientSession`, only available inside the context manager"""
+        if self.__session is None:
+            raise RuntimeError(
+                f"{self.__class__.__qualname__}._session called outside the context manager"
+            )
+        return self.__session
+
+    @property
+    def _semaphore(self) -> asyncio.Semaphore:
+        """private `asyncio.Semaphore`, only available inside the context manager"""
+        if self.__semaphore is None:
+            raise RuntimeError(
+                f"{self.__class__.__qualname__}._semaphore called outside the context manager"
+            )
+        return self.__semaphore
 
     async def country(self) -> CountryResponse:
         """get request to /Country"""
-        assert self.session is not None, "outside context manager"
-        async with self.session.get(self.base_url + "/Country") as r:
-            r.raise_for_status()
-            return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
+        async with self._semaphore:
+            async with self._session.get(f"{self.base_url}/Country") as r:
+                r.raise_for_status()
+                return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
 
     async def property(self) -> PropertyResponse:
         """get request to /Property"""
-        assert self.session is not None, "outside context manager"
-        async with self.session.get(self.base_url + "/Property") as r:
-            r.raise_for_status()
-            return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
+        async with self._semaphore:
+            async with self._session.get(f"{self.base_url}/Property") as r:
+                r.raise_for_status()
+                return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
 
     async def city(self, data: tuple[str, ...]) -> CityResponse:
         """post request to /City"""
-        assert self.session is not None, "outside context manager"
-        async with self.session.post(self.base_url + "/City", json=data) as r:
-            r.raise_for_status()
-            return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
+        async with self._semaphore:
+            async with self._session.post(
+                f"{self.base_url}/City", json=data
+            ) as r:
+                r.raise_for_status()
+                return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
 
     async def download_summary(
         self, data: ParquetDataRequest
     ) -> DownloadSummaryResponse:
         """post request to /DownloadSummary"""
-        assert self.session is not None, "outside context manager"
-        async with self.session.post(
-            self.base_url + "/DownloadSummary", json=data
-        ) as r:
-            r.raise_for_status()
-            return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
+        async with self._semaphore:
+            async with self._session.post(
+                f"{self.base_url}/DownloadSummary", json=data
+            ) as r:
+                r.raise_for_status()
+                return await r.json(encoding="UTF-8")  # type:ignore[no-any-return]
 
     async def download_urls(self, data: ParquetDataRequest) -> str:
         """post request to /ParquetFile/urls"""
-        assert self.session is not None, "outside context manager"
-        async with self.session.post(
-            self.base_url + "/ParquetFile/urls", json=data
-        ) as r:
-            r.raise_for_status()
-            return await r.text(encoding="UTF-8")  # type:ignore[no-any-return]
+        async with self._semaphore:
+            async with self._session.post(
+                f"{self.base_url}/ParquetFile/urls", json=data
+            ) as r:
+                r.raise_for_status()
+                return await r.text(encoding="UTF-8")  # type:ignore[no-any-return]
 
     async def download_binary(self, url: str, path: Path) -> Path:
         """get request to `url`, write response body content (in binary form) into a a binary file,
         and return `path` (exactly as the input)"""
-
-        assert self.session is not None, "outside context manager"
-        assert self.semaphore is not None, "outside context manager"
-        async with self.semaphore:
-            async with self.session.get(url) as r:
+        async with self._semaphore:
+            async with self._session.get(url) as r:
                 r.raise_for_status()
                 payload = await r.read()
             async with aiofiles.open(path, mode="wb") as f:
