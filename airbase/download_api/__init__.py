01b96612885a46f4c72624caed3562ab19c714d7
Alvaro Valdebenito <alvarov@met.no>
2024-09-03 15:10:03 +0200

    explicit allowed re-exports

--- airbase/download_api/__init__.py
+++ airbase/download_api/__init__.py
@@ -6,3 +6,13 @@ from .dataset import (
     request_info_by_country,
 )
 from .session import DownloadSession, download
+
+__all__ = [
+    "Client",
+    "Dataset",
+    "ParquetData",
+    "request_info_by_city",
+    "request_info_by_country",
+    "DownloadSession",
+    "download",
+]
