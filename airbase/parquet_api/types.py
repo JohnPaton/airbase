e1110f5b94d7b6a78116dce6d8d8bcba370ce44b
Alvaro Valdebenito <alvarov@met.no>
2024-09-05 09:44:51 +0200

    typing

--- airbase/parquet_api/types.py
+++ airbase/parquet_api/types.py
@@ -6,12 +6,15 @@ https://eeadmz1-downloads-api-appservice.azurewebsites.net/swagger/index.html
 from __future__ import annotations
 
 import sys
-from typing import TypedDict
+from typing import TYPE_CHECKING, Literal, TypedDict
 
 if sys.version_info >= (3, 10):  # pragma:no cover
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias  # pragma:no cover
+
+if TYPE_CHECKING:
+    from .dataset import Dataset
 
 
 class CityData(TypedDict):
@@ -41,7 +44,7 @@ class ParquetDataJSON(TypedDict):
     countries: list[str]
     cities: list[str]
     properties: list[str]
-    datasets: list[int]
+    datasets: list[Literal[0, 1, 2] | Dataset]
     source: str
 
 
