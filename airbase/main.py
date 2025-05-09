b3df234bf10b60d17f8eab011f9c303a07e80dec
Alvaro Valdebenito <alvarov@met.no>
2025-05-09 12:07:53 +0200

    CLI pluggin as package entry-point

--- airbase/main.py
+++ airbase/main.py
@@ -1,1 +1,22 @@
-from .cli import main  # noqa: F401
+import sys
+from importlib import metadata
+
+import typer
+
+from .cli import main
+
+if sys.version_info >= (3, 10):
+    ENTRY_POINTS = {
+        ep.name: ep.load() for ep in metadata.entry_points(group="airbase.cli")
+    }
+else:
+    ENTRY_POINTS = {
+        ep.name: ep.load()
+        for ep in metadata.entry_points().get("airbase.cli", [])
+    }
+
+for name, plugin in ENTRY_POINTS.items():
+    if isinstance(plugin, typer.Typer):
+        main.add_typer(plugin, name=name)
+    else:
+        main.command(name=name)(plugin)
