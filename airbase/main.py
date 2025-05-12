5780c8ca11ce588549c80415c8d47eb2e3293649
Alvaro Valdebenito <alvarov@met.no>
2025-05-12 13:05:37 +0200

    hid test entry_points

--- airbase/main.py
+++ airbase/main.py
@@ -1,22 +1,17 @@
-import sys
 from importlib import metadata
 
 import typer
 
 from .cli import main
 
-if sys.version_info >= (3, 10):
-    ENTRY_POINTS = {
-        ep.name: ep.load() for ep in metadata.entry_points(group="airbase.cli")
-    }
-else:
-    ENTRY_POINTS = {
-        ep.name: ep.load()
-        for ep in metadata.entry_points().get("airbase.cli", [])
-    }
+ENTRY_POINTS = {
+    ep.name: ep.load() for ep in metadata.entry_points(group="airbase.cli")
+}
+
+TEST_ENTRY_POINTS = {"plugin-sub", "plugin-cmd"}
 
 for name, plugin in ENTRY_POINTS.items():
     if isinstance(plugin, typer.Typer):
-        main.add_typer(plugin, name=name)
+        main.add_typer(plugin, name=name, hidden=name in TEST_ENTRY_POINTS)
     else:
-        main.command(name=name)(plugin)
+        main.command(name=name, hidden=name in TEST_ENTRY_POINTS)(plugin)
