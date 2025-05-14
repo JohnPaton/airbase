91c5b77da5e2f64e704ea495e990dd07632d7195
Alvaro Valdebenito <alvarov@met.no>
2025-05-14 14:15:07 +0200

    full-typer w/rich as extra

--- airbase/cli.py
+++ airbase/cli.py
@@ -204,7 +204,6 @@ PathOption: TypeAlias = Annotated[
         writable=True,
         metavar="PATH/dataset/",
         help="Override dataset donwload path.",
-        show_default=False,
         callback=check_path,
     ),
 ]
